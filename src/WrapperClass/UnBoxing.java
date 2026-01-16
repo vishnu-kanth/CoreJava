4099046582e826b7c51f9e0070432c28001c3efe
VishnuKanth <vishnukanth512@gmail.com>
2026-01-16 11:28:29 +0530

    vjv

---
+++ src/WrapperClass/UnBoxing.java
@@ -0,0 +1,29 @@
+package WrapperClass;
+
+public class UnBoxing {
+	public static void main(String[] args) {
+		// Boxing
+		int a = 10;
+		Integer i = Integer.valueOf(a);
+
+		// UnBoxing
+		int b = i.intValue();
+		System.out.println(b);
+
+		// Byte Boxing
+		byte b1 = 50;
+		Byte b2 = Byte.valueOf(b1);
+		// Unboxing Byte
+
+		byte c = b2.byteValue();
+		System.out.println(c);
+
+		// AutoBoxing
+
+		int s = 77;
+		Integer d = s;
+		int f = d;
+		System.out.println(f);
+		
+	}
+}
